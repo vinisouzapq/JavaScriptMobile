b6889dd152c655c636b1bcdae336a3e044695281
“mvinicius112015@gmail.com” <mvinicius112015@gmail.com>
2020-03-26 22:09:04 -0300

    Primeiro Commit

---
+++ Ex3/index.js
@@ -0,0 +1,16 @@
+function vibracao() {
+
+    navigator.vibrate(450);
+
+}
+
+function vibracaoDelay() {
+
+    navigator.vibrate([100, 150, 200, 250]);
+}
+
+function vibracaoTerceiro() {
+
+    navigator.vibrate(500);
+
+}
