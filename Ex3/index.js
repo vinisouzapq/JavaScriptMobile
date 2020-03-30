310aa8e1e718090822843d8291829520eb690253
“mvinicius112015@gmail.com” <mvinicius112015@gmail.com>
2020-03-29 22:58:00 -0300

    Correção de código

--- Ex3/index.js
+++ Ex3/index.js
@@ -1,29 +1,16 @@
-function vibracao() {
-
+function vibrar1() {
     console.log("Primeira vibração")
-
     navigator.vibrate('450');
-
     console.log("Fim da primeira vibração")
 }
-
-function vibracaoDelay() {
-
+function vibrar2() {
     console.log("Segunda Vibração")
-
     navigator.vibrate(['100', '150', '200', '250']);
-
     console.log("Fim da segunda vibração")
 }
-
-function vibracaoTerceiro() {
-
+function vibrar3() {
     console.log("Terceira vibração")
-
     navigator.vibrate(['250', '200', '150', '100']);
-
     console.log("Fim da terceira vibração")
 }
-
-
 //Funções para fazer o dispositivo vibrar
